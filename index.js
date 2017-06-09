d70aa7911d35211e48c1a140d3a3508033fd0afa
Paul R. Bahler <paul@isnotnil.net>
2017-06-09 19:13:55 -0400

    attempting basic sql read

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 var johnyFive = require('johnny-five');
+var sql = require('msssql');
+
 
 var board = new johnyFive.Board();
+
+const dbConfig = {
+    user: 'sa',
+    password: 'P@55w0rd',
+    server: 'localhost',
+    database: 'SpaceBallon'
+};
+
+var connectionPool = sql.ConnectionPool(config, function (err) {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+
+    pool.request.query('select * from dbo.InternalSensorMeasurements', function (err, results) {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+
+        console.log('got data!')
+    });
+});
 
 board.on('ready', function () {
     var tempInternal = new johnyFive.Thermometer({
