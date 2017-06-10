6e3b11912bd326ca6de905c86ad1ef008b8dccbd
Paul R. Bahler <paul@isnotnil.net>
2017-06-09 20:51:46 -0400

    first crack at Accelerometer

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const createBoard = require('./lib/createBoard');
 const InternalThermometer = require('./lib/InternalThermometer');
 const InternalThermometerDataRecorder = require('./lib/InternalThermometerDataRecorder');
 
+const Accelerometer = require('./lib/Accelerometer');
+
 getConnectionPool(function (connectionPool) {
 
     createBoard(function () {
@@ -15,9 +17,15 @@ getConnectionPool(function (connectionPool) {
         const internalThermometerDataRecorder = 
             new InternalThermometerDataRecorder(connectionPool);
 
+        const accelerometer = new Accelerometer();
+
         internalThermometer.onDataChange(function (data) {
             internalThermometerDataRecorder.recordData(data);
         });
+
+        accelerometer.onDataChange(function (data) {
+            console.log(data);
+        })
 
         internalThermometer.run();
     });
