a887ae14da58fab6d993eb301cca4607c8d3e11b
Paul R. Bahler <paul@isnotnil.net>
2017-06-09 20:32:36 -0400

    refactored to data recording classes

--- index.js
+++ index.js
@@ -4,35 +4,17 @@ const sql = require('mssql');
 
 const getConnectionPool = require('./lib/getConnectionPool');
 const createBoard = require('./lib/createBoard'); 
+
 const InternalThermometer = require('./lib/InternalThermometer');
+const InternalThermometerDataRecorder = require('./lib/InternalThermometerDataRecorder');
 
 getConnectionPool(function (connectionPool) {
 
     createBoard(function () {
         const internalThermometer = new InternalThermometer();
+        const internalThermometerDataRecorder = new InternalThermometerDataRecorder();
 
-        internalThermometer.onDataChange(function (data) {
-            console.log(data.celsius);
-            console.log(data.fahrenheit);
-
-            var query = 'INSERT INTO dbo.InternalSensorMeasurements ' + 
-                '(Celsius, Fahrenheit, CaptureTime) ' + 
-                'VALUES ' + 
-                '(@Celsius, @Fahrenheit, @CaptureTime)';
-
-            connectionPool.request()
-                .input('Celsius', sql.Numeric(7, 2), data.celsius)
-                .input('Fahrenheit', sql.Numeric(7, 2), data.fahrenheit)
-                .input('CaptureTime', sql.DateTime2, data.captureTime)
-                .query(query, function (err, results) {
-                    if (err) {
-                        console.log(err);
-                        throw err;
-                    }
-
-                    console.log('inserted data!')
-                });
-        });
+        internalThermometer.onDataChange(internalThermometerDataRecorder.recordData)
 
         internalThermometer.run();
     });
