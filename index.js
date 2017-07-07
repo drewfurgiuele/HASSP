43c41fa0da03b91edbbcecd56df1461807963bce
Paul Bahler <paul@isnotnil.net>
2017-07-07 14:58:32 -0400

    more comments

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ getConnectionPool(function (connectionPool) {
         const accelerometerDataRecorder = 
             new AccelerometerDataRecorder(connectionPool);
 
-        const digitalPressureSensorOne = new DigitalPressureSensor180();
+        //const digitalPressureSensorOne = new DigitalPressureSensor180();
         const digitalPressureSensorTwo = new DigitalPressureSensor280();
         const externalSensorDataRecorder = new ExternalSensorDataRecorder(connectionPool);
 
-        const gpsSensor = new GpsSensor();
-        const gpsSensorDataRecorder = new GpsSensorDataRecorder(connectionPool);
+        //const gpsSensor = new GpsSensor();
+        //const gpsSensorDataRecorder = new GpsSensorDataRecorder(connectionPool);
 
         const led = new Led();
 
