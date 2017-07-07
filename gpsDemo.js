e9c1232fc0c1f486b130b75be3999cc9b0a95f24
Paul Bahler <paul@isnotnil.net>
2017-07-07 15:03:30 -0400

    added gps demo file

---
+++ gpsDemo.js
@@ -0,0 +1,24 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  /*
+   * This is the simplest initialization
+   * We assume SW_SERIAL0 for the port
+   */
+  var gps = new five.GPS({
+    breakout: "ADAFRUIT_ULTIMATE_GPS",
+    pins: {
+      rx: 11,
+      tx: 10,
+    }
+  });
+
+  // If latitude, longitude, course or speed change log it
+  gps.on("change", function() {
+    console.log("position");
+    console.log("  latitude   : ", this.latitude);
+    console.log("  longitude  : ", this.longitude);
+    console.log("--------------------------------------");
+  });
+});
